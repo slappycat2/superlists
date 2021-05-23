b563a4b4a394b5f05b6d78065354b463dee62f40
slappycat2 <slarsen2@gmail.com>
2021-05-23 08:23:34 -0400

    Switched tousing unittets, setup Class and extended user story

--- book-example-master/book-example-chapter_02_unittest/functional_tests.py
+++ book-example-master/book-example-chapter_02_unittest/functional_tests.py
@@ -1,5 +1,5 @@
-import unittest
 from selenium import webdriver
+import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -9,13 +9,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_starting_a_new_todo_list(self):
-        # Edith has heard about a cool new to-do list app. She goes
-        # to checkout its homepage.
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
@@ -38,6 +39,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':
-    unittest.main()
 
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
